a0e458953bbd13e9ab2021b55f7bb99ab1c37e80
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-04-30 12:28:11 +0200

    travel list + travel item

---
+++ src/app/travel-feed/travel-list/travel-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Travel } from '../../shared/travel.model';
+
+@Component({
+  selector: 'app-travel-list',
+  templateUrl: './travel-list.component.html',
+  styleUrl: './travel-list.component.css',
+})
+export class TravelListComponent {
+  @Output() travelWasSelected = new EventEmitter<Travel>();
+  travels: Travel[] = [
+    new Travel(
+      'Paris, France',
+      7,
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Tour_Eiffel_7.jpg/220px-Tour_Eiffel_7.jpg',
+      new Date('2024-05-15'),
+      new Date('2024-05-22'),
+      'John Doe',
+      'Exciting trip to explore the beautiful city of Paris!'
+    ),
+    new Travel(
+      'Tokyo, Japan',
+      10,
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Tour_Eiffel_7.jpg/220px-Tour_Eiffel_7.jpg',
+      new Date('2024-06-10'),
+      new Date('2024-06-20'),
+      'Jane Smith',
+      'Unforgettable journey to experience the vibrant culture of Tokyo.'
+    ),
+  ];
+
+  onTravelSelected(travel: Travel) {
+    this.travelWasSelected.emit(travel);
+  }
+}
