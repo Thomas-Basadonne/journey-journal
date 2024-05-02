06aa71a32da7cd113a78c1b782575c1593e23f97
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2024-05-02 12:24:55 +0200

    edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TravelFeedComponent } from './travel-feed/travel-feed.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { TravelDetailComponent } from './travel-feed/travel-detail/travel-detail.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/travel-feed', pathMatch: 'full' },
   { path: 'travel-feed', component: TravelFeedComponent },
   { path: 'travel-detail/:id', component: TravelDetailComponent },
   { path: 'dashboard', component: UserDashboardComponent },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { AuthComponent } from './auth/auth.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { TravelFeedComponent } from './travel-feed/travel-feed.component';
 import { TravelListComponent } from './travel-feed/travel-list/travel-list.component';
@@ -16,6 +16,8 @@ import { TravelItemComponent } from './travel-feed/travel-list/travel-item/trave
 import { TravelService } from './travel-feed/travel.service';
 import { TruncatePipe } from './shared/truncate.pipe';
 import { UserTravelListComponent } from './user-dashboard/user-travel-list/user-travel-list.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -30,12 +32,14 @@ import { UserTravelListComponent } from './user-dashboard/user-travel-list/user-
     TravelItemComponent,
     TruncatePipe,
     UserTravelListComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [TravelService],
   bootstrap: [AppComponent],
